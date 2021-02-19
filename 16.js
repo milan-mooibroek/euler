f922dc8b7b8d85aa9a4333001333f8e77a0b2546
milan-mooibroek <mooibroekmilan@gmail.com>
2021-02-19 23:16:57 +0100

    code cleanup.

--- 16.js
+++ 16.js
@@ -1,4 +1,2 @@
 const powerSum = BigInt(Math.pow(2, 1000));
-let summedTotal = 0;
-
 console.log(`the summerd op total is ${String(powerSum).split('').reduce((a, b) => parseInt(a) + parseInt(b), 0)}`);
